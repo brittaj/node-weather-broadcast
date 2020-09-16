3d6d17ccd0762f7c4c690d979a256859f08883c6
brittaj <britta.alex@gmail.com>
2020-09-16 09:02:11 +0800

    added humidity param

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (lat,lon,callback)=>{
             callback(`Unable to find location`,undefined)
         }
         else{
-            callback(undefined,`Location is ${response.body.location.name}. ${response.body.current.weather_descriptions}. It is currently ${response.body.current.temperature} degrees out. There is ${response.body.current.precip}% chance of rain. The temperature is ${response.body.current.temperature}. But it feels like ${response.body.current.feelslike}.`)
+            callback(undefined,`Location is ${response.body.location.name}. ${response.body.current.weather_descriptions}. It is currently ${response.body.current.temperature} degrees out. There is ${response.body.current.precip}% chance of rain. The temperature is ${response.body.current.temperature}. But it feels like ${response.body.current.feelslike}. The humidity is ${response.body.current.humidity}.`)
         }
     })
 }
